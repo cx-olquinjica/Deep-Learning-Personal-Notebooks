0f46e7feb6c154a50a36407865d640ef7f0813e6
cx-olquinjica <aosiwaduo@outlook.com>
2023-07-02 14:34:04 +0800

    rebase:error

---
+++ attention-transformers/model.py
@@ -0,0 +1,65 @@
+#! /Users/admin/miniconda3/envs/d2l/bin/python
+
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class RNNModel(nn.Module):
+    """Container module with an enconder, a recurrent module, and a decoder."""
+
+    def __init__(self, rnn_type, ntoken, ninp, nhid, nlayers, dropout=0.5, tie_weights=False):
+        super(RNNModel, self).__init__()
+        self.ntoken = ntoken
+        self.drop = nn.Dropout(dropout)
+        self.encoder = nn.Embedding(ntoken, ninp)
+        if rnn_type in ['LSTM', 'GRU']:
+            self.rnn = getattr(nn, rnn_type)(ninp, nhid, nlayers, dropout=droupout)
+        else:
+            try:
+                nonlinearity = {'RNN_TANH':'tanh', 'RNN_RELU': 'relu'}[rnn_type]
+            except KeyrError as e:
+                raise ValueError( """An invalid option for '--model' was suplied, 
+                                    options are ['LSTM', 'GRU', 'RNN_TANH' or 'RNN_RELU']""") from e
+            self.rnn = nn.RNN(ninp, nhid, nlayers, nonlinearity=nonlinearity, dropout=droupout)
+        self.decoder = nn.Linear(nhid, ntoken)
+
+        # Optionally tie weights as in:
+        # "Using the Output Embedding to Improve Language Models" (Press & Wolf 2016)
+        # https://arvix.org/abs/1608.05859
+
+        if tie_weights:
+            if nhid != ninp:
+                raise ValueError('When using the tied flag, nhid must be equal to emsize')
+            self.decoder.weight = self.encoder.weight
+
+        self.init_weights()
+
+        self.rnn_type = rnn_type
+        self.nhid = nhid
+        self.nlayers = nlayers
+
+        def init_weights(self):
+            initrange = 0.1
+            nn.init.uniform_(self.encoder.weight, -initrange, initrange)
+            nn.init.zeros_(self.decoder.bias)
+            nn.init.uniform_(self.decoder.weight, -initrange, initrange)
+
+        def forward(self, input, hidden):
+            emb = self.drop(self.encoder(input))
+            output, hidden = self.rnn(emb, hidden)
+            output = self.drop(output)
+            decoded = self.decoder(output)
+            decoded = decoded.view(-1, self.ntoken)
+            return F.log_softmax(decoded, dim=1), hidden
+
+        def init_hidden(self, bsz):
+            weight = next(self.parameters())
+            if self.rnn_type == 'LSTM':
+                return (weight.new_zeros(self.nlayers, bsz, self.nhid), 
+                        weight.new_zeros(self.nlayers, bsz, self.nhid))
+            else:
+                return weight.new_zeros(self.nlayers, bsz, self.nhid)
+
+
