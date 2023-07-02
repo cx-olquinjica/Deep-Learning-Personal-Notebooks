a9ff5657b57b83abecf1c5dc5af964ee2bef29a2
cx-olquinjica <aosiwaduo@outlook.com>
2023-07-02 15:20:22 +0800

    fix indentation error

--- attention-transformers/model.py
+++ attention-transformers/model.py
@@ -15,14 +15,14 @@ class RNNModel(nn.Module):
         self.drop = nn.Dropout(dropout)
         self.encoder = nn.Embedding(ntoken, ninp)
         if rnn_type in ['LSTM', 'GRU']:
-            self.rnn = getattr(nn, rnn_type)(ninp, nhid, nlayers, dropout=droupout)
+            self.rnn = getattr(nn, rnn_type)(ninp, nhid, nlayers, dropout=dropout)
         else:
             try:
                 nonlinearity = {'RNN_TANH':'tanh', 'RNN_RELU': 'relu'}[rnn_type]
-            except KeyrError as e:
+            except KeyError as e:
                 raise ValueError( """An invalid option for '--model' was suplied, 
                                     options are ['LSTM', 'GRU', 'RNN_TANH' or 'RNN_RELU']""") from e
-            self.rnn = nn.RNN(ninp, nhid, nlayers, nonlinearity=nonlinearity, dropout=droupout)
+            self.rnn = nn.RNN(ninp, nhid, nlayers, nonlinearity=nonlinearity, dropout=dropout)
         self.decoder = nn.Linear(nhid, ntoken)
 
         # Optionally tie weights as in:
@@ -40,26 +40,26 @@ class RNNModel(nn.Module):
         self.nhid = nhid
         self.nlayers = nlayers
 
-        def init_weights(self):
-            initrange = 0.1
-            nn.init.uniform_(self.encoder.weight, -initrange, initrange)
-            nn.init.zeros_(self.decoder.bias)
-            nn.init.uniform_(self.decoder.weight, -initrange, initrange)
+    def init_weights(self):
+        initrange = 0.1
+        nn.init.uniform_(self.encoder.weight, -initrange, initrange)
+        nn.init.zeros_(self.decoder.bias)
+        nn.init.uniform_(self.decoder.weight, -initrange, initrange)
 
-        def forward(self, input, hidden):
-            emb = self.drop(self.encoder(input))
-            output, hidden = self.rnn(emb, hidden)
-            output = self.drop(output)
-            decoded = self.decoder(output)
-            decoded = decoded.view(-1, self.ntoken)
-            return F.log_softmax(decoded, dim=1), hidden
+    def forward(self, input, hidden):
+        emb = self.drop(self.encoder(input))
+        output, hidden = self.rnn(emb, hidden)
+        output = self.drop(output)
+        decoded = self.decoder(output)
+        decoded = decoded.view(-1, self.ntoken)
+        return F.log_softmax(decoded, dim=1), hidden
 
-        def init_hidden(self, bsz):
-            weight = next(self.parameters())
-            if self.rnn_type == 'LSTM':
-                return (weight.new_zeros(self.nlayers, bsz, self.nhid), 
-                        weight.new_zeros(self.nlayers, bsz, self.nhid))
-            else:
-                return weight.new_zeros(self.nlayers, bsz, self.nhid)
+    def init_hidden(self, bsz):
+        weight = next(self.parameters())
+        if self.rnn_type == 'LSTM':
+            return (weight.new_zeros(self.nlayers, bsz, self.nhid), 
+                 weight.new_zeros(self.nlayers, bsz, self.nhid))
+        else:
+            return weight.new_zeros(self.nlayers, bsz, self.nhid)
 
 
